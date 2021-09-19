2230a8d62c18db239a288522277854015495ba94
memeToasty <memeToasty@gmail.com>
2021-09-19 18:08:12 +0200

    some minor adjustments 👌

--- main.py
+++ main.py
@@ -1,39 +1,81 @@
 from PIL import Image, ImageDraw, ImageFont
-import sys
 import json
+import argparse
+from io import BytesIO
+import win32clipboard
 
-config = json.load(open("config.json", 'r'))
+def send_to_clipboard(image):
+    output = BytesIO()
+    image.convert('RGB').save(output, 'BMP')
+    data = output.getvalue()[14:]
+    output.close()
+
+    win32clipboard.OpenClipboard()
+    win32clipboard.EmptyClipboard()
+    win32clipboard.SetClipboardData(win32clipboard.CF_DIB, data)
+    win32clipboard.CloseClipboard()
+
+_types = {
+    'str': str,
+    'int': int,
+    'float': float,
+    'complex': complex,
+    'bool': bool,
+}
+
+with open('templates.json', 'r') as f:
+    templates = json.load(f)
+
+with open('schema.json', 'r') as f:
+    arguments = json.load(f)
 
 
-if sys.argv[1] == "help":
-    print(
-    """
-    [---- Fast Meme Generator v1.0 ----]
-    Usage:
-        python main.py MEMETEMPLATE TEXT [FontSize]
+parser = argparse.ArgumentParser(description=arguments["description"])
 
-        MEMETEMPLATE:
-            monke
+for argument in arguments["arguments"]:
+    #optional Argument properties
+    if 'default' in argument:
+        parser.add_argument(
+            '--' + argument['long'],
+            help = argument['help'],
+            default = argument['default'],
+            type = _types[argument['type']],
+            metavar = argument['var'],
+            required = False
+        )
+    
+    else:
+        # required Argument properties
+        parser.add_argument(
+            '--' + argument['long'],
+            help = argument['help'],
+            type = _types[argument['type']],
+            metavar = argument['var'],
+            required = True
+        )
 
-    """)
-    exit()
+args = parser.parse_args()
 
+# Command line argument assertion
 
+template = args.template
+fontSize = args.font_size
+printingText = args.text
+fontPath = args.font
 
-printingText = ' '.join([str(v) for v in sys.argv[2:len(sys.argv)]])
-fontSize = sys.argv[1]
+font = ImageFont.truetype(fontPath, int(fontSize))
+image = Image.open(templates[template]['path'])
+imageDraw = ImageDraw.Draw(image)
 
-template = Image.open('res/monke.png')
-font = ImageFont.truetype("arial.ttf", int(fontSize))
-d1 = ImageDraw.Draw(template)
-
-d1.text(
-    (1200,70),
+imageDraw.text(
+    (templates[template]['pos'][0],templates[template]['pos'][1]),
     printingText,
     fill = (0, 0, 0),
     font = font,
     anchor = 'mm'
 )
+
+# TODO: Refactor
 def alNumify(input_string):
     final_string = ""
     for character in input_string:
@@ -42,4 +84,7 @@ def alNumify(input_string):
             final_string = final_string + character
     return final_string
 
-template.save(alNumify(printingText) + ".png")
+image.save(alNumify(printingText) + ".png")
+
+send_to_clipboard(image)
+
